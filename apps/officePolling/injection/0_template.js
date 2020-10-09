b3ea1cba55642d14d0e9469ecfb6b98de01fe208
homeliaison <homeliaisonphoto@gmail.com>
2020-10-09 15:46:22 +0900

    Robot_201009_v12

--- apps/officePolling/injection/0_template.js
+++ apps/officePolling/injection/0_template.js
@@ -1,19 +1,5 @@
 /*
 
-Result Folder :
-  inClass => this.dir (last \ exist)
-  Local => __defaultResultFolder__ (last \ no)
-
-ExecMain.prototype.launching = async function () {
-  try {
-
-  } catch (e) {
-    alert(e);
-  }
-}
-
-const aiInstance = new ExecMain({}, __defaultResultFolder__, {});
-aiInstance.launching();
-app.quit();
+nothing
 
 */
