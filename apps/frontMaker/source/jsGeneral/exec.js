4d1324deaf31e6bf3d45f7609964b960c5687eaf
homeliaison <homeliaisonphoto@gmail.com>
2020-10-10 21:48:43 +0900

    Robot_201010_v6

--- apps/frontMaker/source/jsGeneral/exec.js
+++ apps/frontMaker/source/jsGeneral/exec.js
@@ -1,10 +1,3 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'UA-97880990-1');
-
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
