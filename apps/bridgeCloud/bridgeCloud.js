112f4dc8e71064007b481907654d10dabe044e58
uragen <uragenbooks@gmail.com>
2021-02-03 17:28:55 +0900

    Robot_210203_v7

--- apps/bridgeCloud/bridgeCloud.js
+++ apps/bridgeCloud/bridgeCloud.js
@@ -446,7 +446,7 @@ BridgeCloud.prototype.bridgeServer = function (needs) {
 
       if (resultObj.mode === "partnership") {
 
-        message = "새로운 디자이너 신청이 왔습니다!\n";
+        message = "새로운 디자이너 신청이 등록되었습니다!\n";
         message += "문의일 : " + dateToString(filteredObj.date) + "\n";
         message += "성함 : " + filteredObj.designer + "\n";
         message += "연락처 : " + filteredObj.phone + "\n";
@@ -471,7 +471,7 @@ BridgeCloud.prototype.bridgeServer = function (needs) {
 
       } else {
 
-        message = "새로운 설명회 신청이 왔습니다!\n";
+        message = "디자이너 설명회 신청이 등록되었습니다!\n";
         message += "문의일 : " + dateToString(filteredObj.date) + "\n";
         message += "성함 : " + filteredObj.designer + "\n";
         message += "연락처 : " + filteredObj.phone + "\n";
