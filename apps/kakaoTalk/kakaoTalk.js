c4849fa94b05fb2ba4b555e18ed7dc4bd872a13d
uragen <uragenbooks@gmail.com>
2021-02-16 15:46:38 +0900

    Robot_210216_v4

--- apps/kakaoTalk/kakaoTalk.js
+++ apps/kakaoTalk/kakaoTalk.js
@@ -89,8 +89,8 @@ KakaoTalk.prototype.templateTong = function (target) {
     pastcomplete: "TA_7532",
     complete: "TC_1244",
     photo: "TC_1179",
-    designerPartnership: "TD_4907",
-    designerPresentation: "TD_4908",
+    designerPartnership: "TD_5890",
+    designerPresentation: "TD_5891",
   };
   return tong[target];
 }
