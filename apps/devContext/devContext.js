f5e09371a7f80d3a205eff019e4df456b1449f04
uragen <uragenbooks@gmail.com>
2021-03-31 11:57:42 +0900

    Robot_210331_v12

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -1765,7 +1765,7 @@ class DevContext extends Array {
 
 
 
-      const response = await requestSystem("https://home-liaison.servehttp.com:8080/shell", { hello: "" });
+      const response = await requestSystem("https://home-liaison.servehttp.com:8080/shell", { command: "ls -al /home" }, { method: "json" });
       console.log(response);
 
 
