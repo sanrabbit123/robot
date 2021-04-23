7b88e1b99c05bb13487232754b7ebbc30775a9ee
uragen <uragenbooks@gmail.com>
2021-04-23 20:26:27 +0900

    Robot_210423_v4

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -413,30 +413,30 @@ DevContext.prototype.launching = async function () {
 
 
     // contents upload
-    const client = "이순주";
-    const pid = "p90";
-    const rid = "re084";
-    const links = [
-      "https://docs.google.com/document/d/1iEx_pkNaWDsRa7buqiBxHiinhuCFntOpsxX1uU9o4OY/edit?usp=sharing",
-      "https://docs.google.com/document/d/1F0_pRTzbffm8kCpe9niUckhAfidSen8n0B10B5dKGzU/edit?usp=sharing",
-      "https://drive.google.com/drive/folders/1NIqB0HZkD6dFRd-y446xKMxP2h8PeZN1?usp=sharing",
-    ];
-    const webLinks = [
-      "https://home-liaison.com/portdetail.php?qqq=" + pid,
-      "https://home-liaison.com/revdetail.php?qqq=" + rid,
-    ];
-    let channel;
-
-    // 1
-    channel = "#502_sns_contents";
-    await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님의 디자이너 포트폴리오 글의 세팅을 완료하였습니다! 확인부탁드립니다. link : ${links[0]}`, channel });
-    await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님의 고객 인터뷰 글의 세팅을 완료하였습니다! 확인부탁드립니다. link : ${links[1]}`, channel });
-    await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님 세팅 사진 원본 link : ${links[2]}`, channel });
-
-    // 2
-    channel = "#200_web";
-    await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님 디자이너 포트폴리오 컨텐츠를 웹에 업로드하였습니다! link : ${webLinks[0]}`, channel });
-    await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님 고객 인터뷰 컨텐츠를 웹에 업로드하였습니다! link : ${webLinks[1]}`, channel });
+    // const client = "이순주";
+    // const pid = "p90";
+    // const rid = "re084";
+    // const links = [
+    //   "https://docs.google.com/document/d/1iEx_pkNaWDsRa7buqiBxHiinhuCFntOpsxX1uU9o4OY/edit?usp=sharing",
+    //   "https://docs.google.com/document/d/1F0_pRTzbffm8kCpe9niUckhAfidSen8n0B10B5dKGzU/edit?usp=sharing",
+    //   "https://drive.google.com/drive/folders/1NIqB0HZkD6dFRd-y446xKMxP2h8PeZN1?usp=sharing",
+    // ];
+    // const webLinks = [
+    //   "https://home-liaison.com/portdetail.php?qqq=" + pid,
+    //   "https://home-liaison.com/revdetail.php?qqq=" + rid,
+    // ];
+    // let channel;
+    //
+    // // 1
+    // channel = "#502_sns_contents";
+    // await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님의 디자이너 포트폴리오 글의 세팅을 완료하였습니다! 확인부탁드립니다. link : ${links[0]}`, channel });
+    // await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님의 고객 인터뷰 글의 세팅을 완료하였습니다! 확인부탁드립니다. link : ${links[1]}`, channel });
+    // await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님 세팅 사진 원본 link : ${links[2]}`, channel });
+    //
+    // // 2
+    // channel = "#200_web";
+    // await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님 디자이너 포트폴리오 컨텐츠를 웹에 업로드하였습니다! link : ${webLinks[0]}`, channel });
+    // await this.mother.slack_bot.chat.postMessage({ text: `${client} 고객님 고객 인터뷰 컨텐츠를 웹에 업로드하였습니다! link : ${webLinks[1]}`, channel });
 
 
     // card setting
