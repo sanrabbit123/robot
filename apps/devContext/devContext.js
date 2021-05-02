a16b2f87196c225186f7f1829fd1068be94f61b3
uragen <uragenbooks@gmail.com>
2021-05-02 22:33:38 +0900

    Robot_210502_v4

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -175,6 +175,7 @@ DevContext.prototype.launching = async function () {
     // kakao token
     const app = new KakaoTalk();
     // await app.generateToken();
+    await app.ready();
     await app.sendTalk("complete", "배창규", "010-2747-3403");
 
 
