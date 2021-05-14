aa98ad18b7cc963049384552d6a1df9037a78802
uragen <uragenbooks@gmail.com>
2021-05-14 23:49:37 +0900

    Robot_210514_v12

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -60,33 +60,6 @@ DevContext.prototype.launching = async function () {
     // console.log(amount, buyer_name, buyer_tel, card_name, name);
 
 
-    for (var i = 0; i < 20; i++) {
-      const payResponse = await requestSystem("https://api.iamport.kr/users/getToken", { "imp_key": "7188483898255321", "imp_secret": "05z9vXYzdvq9Xb2SHBu8j8RpTw60LnALs9UY6TxkoYul9weR8JZsSRSLoYM9lmUOwPMCIjX7istrYIj7" }, { headers: { "Content-Type": "application/json" } });
-      const token = payResponse.data.response.access_token;
-      const { data } = await requestSystem("https://api.iamport.kr/payments/imp_706381046289", {}, { headers: { "X-ImpTokenHeader": token } });
-      const { amount, buyer_name, buyer_tel, card_name, name } = data.response;
-      const clients = await back.getClientsByQuery({ phone: buyer_tel });
-      let client, cliid;
-      let projects;
-      let whereQuery, updateQuery;
-
-      if (clients.length === 1) {
-        client = clients[0];
-        cliid = client.cliid;
-        projects = await back.getProjectsByQuery({ $and: [ { cliid }, { desid: { $regex: "^d" } } ] });
-        if (projects.length > 0) {
-          whereQuery = { proid: projects[0].proid };
-          if (/계/gi.test(name)) {
-            updateQuery = { "process.contract.first.date": new Date() };
-          } else {
-            updateQuery = { "process.contract.remain.date": new Date() };
-          }
-          console.log(whereQuery, updateQuery);
-        }
-      }
-    }
-
-
 
 
     // const aspirants = await back.getAspirantsByQuery({});
@@ -157,8 +130,8 @@ DevContext.prototype.launching = async function () {
 
 
     // get drive folder
-    // const drive = new GoogleDrive();
-    // await drive.get_folder("https://drive.google.com/drive/folders/1EPqtpQeY_JOeKN4gtskh9sm6H8eOdSKd");
+    const drive = new GoogleDrive();
+    await drive.get_folder("https://drive.google.com/drive/folders/1Syjhfd-vm4ZrTuCuuwLKrJYOPQIMISc-");
 
 
     // naverBlog to json
