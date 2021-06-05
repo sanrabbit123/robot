39daa6d95ac0e15cd4d25f70031590733fbf9d19
uragen <uragenbooks@gmail.com>
2021-06-05 17:48:48 +0900

    Robot_210605_v11

--- robot.js
+++ robot.js
@@ -425,10 +425,11 @@ Robot.prototype.staticInSync = async function () {
     }
 
     order = "scp -r ";
-    order += this.address.homeinfo.ghost.user + "@" + this.address.homeinfo.ghost.inner + ":" + shellLink(this.address.homeinfo.ghost.file.static);
+    order += this.address.homeinfo.ghost.user + "@" + this.address.homeinfo.ghost.host + ":" + shellLink(this.address.homeinfo.ghost.file.static);
     order += " ";
     order += shellLink(home + "/" + driveName);
 
+    console.log(order);
     shell.exec(order);
 
     return home + "/" + driveName;
