7dc1ddb6ace6e9de8894b0d73ba6459f9ec336fa
uragen <uragenbooks@gmail.com>
2021-06-12 11:11:19 +0900

    Robot_210612_v2

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -60,6 +60,52 @@ DevContext.prototype.launching = async function () {
 
 
 
+    const map = instance.address.officeinfo.map;
+    const port = 3000;
+    let target;
+    target = null;
+    for (let obj of map) {
+      if (obj.name === "graphic") {
+        target = obj.ip;
+        break;
+      }
+    }
+    if (target !== null) {
+      target = "http://" + target + ":" + String(port);
+      headRequest(target + "/confirm").then(async (response) => {
+        const { statusCode } = response;
+        let raw, res, doing;
+        if (statusCode === 200) {
+          raw = await requestSystem(target + "/confirm");
+          if (raw.data.doing !== undefined) {
+            await sleep(1000);
+            doing = raw.data.doing;
+            while (doing === 1) {
+              console.log("waiting...");
+              await sleep(1000);
+              raw = await requestSystem(target + "/confirm");
+              if (raw.data.doing !== undefined) {
+                doing = raw.data.doing;
+              } else {
+                doing = 2;
+              }
+            }
+            if (doing === 0) {
+              res = await requestSystem(target + "/print");
+              console.log("success");
+              // await slack_bot.chat.postMessage({ text: "프린트 출력을 완료하였습니다!", channel: "#401_consulting" });
+              console.log("print", res.data);
+            }
+          }
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+
+
+
+
 
     /*
 
