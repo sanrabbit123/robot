9050065f7c21a1e0d3bdcbbb444a35443897fc95
uragen <uragenbooks@gmail.com>
2021-06-17 15:07:35 +0900

    Robot_210617_v22

--- apps/receiptObserver/router/receiptRouter.js
+++ apps/receiptObserver/router/receiptRouter.js
@@ -254,6 +254,7 @@ ReceiptRouter.prototype.rou_post_cashReceipt = function () {
         for (let obj of cashIn) {
           rows = await back.mongoRead(collection, { $and: [ { method: 1 }, { id: obj.id } ] }, { selfMongo });
           if (rows.length === 0) {
+            console.log(obj);
             await slack_bot.chat.postMessage({ text: obj.toMessage(), channel: "#701_taxbill" });
             await back.mongoCreate(collection, obj, { selfMongo: instance.mongolocal });
           }
