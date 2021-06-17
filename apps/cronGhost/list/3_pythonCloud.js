1976b9201a889657943300f5a1319b7efe58aeae
uragen <uragenbooks@gmail.com>
2021-06-17 15:42:35 +0900

    Robot_210617_v27

--- apps/cronGhost/list/3_pythonCloud.js
+++ apps/cronGhost/list/3_pythonCloud.js
@@ -8,6 +8,15 @@ module.exports = [
     }
   },
   {
+    name: "cashReceipt",
+    time: {
+      day_of_week: "mon",
+      hour: 18,
+      minute: 10,
+      second: 10,
+    }
+  },
+  {
     name: "designerCalculation",
     time: {
       day_of_week: "mon",
