7faabe54e574b740a250b697c4bf23663220d6e7
uragen <uragenbooks@gmail.com>
2021-06-24 17:26:19 +0900

    Robot_210624_v60

--- apps/graphicBot/list/1_eform.js
+++ apps/graphicBot/list/1_eform.js
@@ -51,7 +51,9 @@ module.exports = function (proid, info) {
       for (let { id, value } of map) {
         await injectionInput(document.getElementById(id), value);
       }
-      await scrollWindow(120, "center", -17.8);
+
+      await sleep(500);
+      await scrollWindow(120, "center", -18);
 
       map = [
         { id: "field_TEXT_5faa618f9da73962a9050ef5", value: "배창규" },
@@ -63,34 +65,29 @@ module.exports = function (proid, info) {
         // { id: "field_TEXT_5faa618f9da73962a9050efd", value: "2020-01-01" },
         // { id: "field_TEXT_5faa618f9da73962a9050efe", value: "2020-01-01" },
         // { id: "field_TEXT_5faa618f9da73962a9050efc", value: "2020-01-01" },
+        { id: "field_TEXT_5faa618f9da73962a9050eff", value: "34평" },
+        { id: "field_TEXT_AREA_5faa618f9da73962a9050f00", value: "방 3개 / 화장실 2개" },
+        { id: "field_TEXT_5faa618f9da73962a9050f03", value: "홈스타일링" },
       ];
 
       for (let { id, value } of map) {
         await injectionInput(document.getElementById(id), value);
       }
-      await scrollWindow(120, "center", -17.8);
 
-      // map = [
-      //   { id: "field_TEXT_5faa618f9da73962a9050eff", value: "34평" },
-      //   { id: "field_TEXT_AREA_5faa618f9da73962a9050f00", value: "방 3개 / 화장실 2개" },
-      //   { id: "field_TEXT_5faa618f9da73962a9050f03", value: "홈스타일링" },
-      // ];
-      //
-      // for (let { id, value } of map) {
-      //   await injectionInput(document.getElementById(id), value);
-      // }
-      // await scrollWindow(120, "center", -17.8);
-      //
-      // map = [
-      //   { id: "field_TEXT_5faa618f9da73962a9050f05", value: "330000" },
-      //   { id: "field_TEXT_5faa618f9da73962a9050f06", value: "33000" },
-      // ];
-      //
-      // for (let { id, value } of map) {
-      //   await injectionInput(document.getElementById(id), value);
-      // }
-      // await scrollWindow(120, "center", -17.8);
-      //
+      await sleep(500);
+      await scrollWindow(120, "center", -16);
+
+      map = [
+        { id: "field_TEXT_5faa618f9da73962a9050f05", value: "330000" },
+        { id: "field_TEXT_5faa618f9da73962a9050f06", value: "33000" },
+      ];
+
+      for (let { id, value } of map) {
+        await injectionInput(document.getElementById(id), value);
+      }
+      await sleep(500);
+      await scrollWindow(120, "center", -50);
+
       // map = [
       //   { id: "field_TEXT_5faa618f9da73962a9050f16", value: "배창규" },
       //   { id: "field_TEXT_5faa618f9da73962a9050f1a", value: "배창규" },
