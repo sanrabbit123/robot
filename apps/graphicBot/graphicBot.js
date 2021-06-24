e3cc5f842fa2a999cfa570ea480f8c90bc874aaa
uragen <uragenbooks@gmail.com>
2021-06-24 15:47:37 +0900

    Robot_210624_v43

--- apps/graphicBot/graphicBot.js
+++ apps/graphicBot/graphicBot.js
@@ -659,7 +659,7 @@ GraphicBot.prototype.botServer = async function () {
 
       if (y >= screenSize.height) {
         robot.moveMouse(screenSize.width / 2, screenSize.height / 2);
-        robot.scrollMouse(0, 900000);
+        robot.scrollMouse(0, y + screenSize.height + chromeHeight);
         indent = (screenSize.height - chromeHeight) / 2;
         robot.scrollMouse(0, (-1 * y) + indent);
         robot.moveMouse(x, indent);
