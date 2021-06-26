4060a5c98c9207378103e4ab42abf67d22ac4379
uragen <uragenbooks@gmail.com>
2021-06-26 16:44:33 +0900

    Robot_210626_v7

--- apps/graphicBot/graphicBot.js
+++ apps/graphicBot/graphicBot.js
@@ -451,7 +451,7 @@ GraphicBot.prototype.botOrders = async function (num, arg) {
       } else if (typeof i === "function") {
         tempString = i.toString().trim().replace(/\}$/, '').replace(/^async function[^\(\)]*\([^\(\)]*\)[^\{]*\{/gi, '');
         tempString = "(async function () {\n\n" + frontFirst + tempString + frontEnd + "\n\n})();";
-        await sleep(1000);
+        await sleep(3000);
         await this.moveAndClick(1622, 1030, 500, false);
         await this.clipBoard(tempString);
         await this.pasteText();
