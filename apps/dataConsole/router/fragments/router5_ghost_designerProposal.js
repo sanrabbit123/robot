529d5ff64515c22d349067f7d3fb938d35037410
uragen <uragenbooks@gmail.com>
2021-07-07 10:39:59 +0900

    Robot_210707_v1

--- apps/dataConsole/router/fragments/router5_ghost_designerProposal.js
+++ apps/dataConsole/router/fragments/router5_ghost_designerProposal.js
@@ -9,7 +9,7 @@ DataRouter.prototype.rou_post_designerProposal_submit = function () {
     try {
       res.set({ "Content-Type": "application/json" });
       let { cliid, proid, desid, name, phone, designer } = req.body;
-      slack_bot.chat.postMessage({ text: `${name} 고객님이 ${designer}(${desid}) 디자이너를 선택하셨습니다! 알림톡이 갔으니 확인 연락 부탁드립니다!\n${name} 고객님 : https://${address.backinfo.host}/client?cliid=${cliid}\n제안서 : https://${address.homeinfo.ghost.host}/middle/proposal?proid=${proid}\n디자이너 : https://${address.backinfo.host}/designer?desid=${desid}`, channel: "#400_customer" });
+      slack_bot.chat.postMessage({ text: `${name} 고객님이 ${designer}(${desid}) 디자이너를 선택하셨습니다! 알림톡이 갔으니 확인 연락 부탁드립니다!\n${name} 고객님 : https://${address.backinfo.host}/client?cliid=${cliid}\n제안서 : https://${address.homeinfo.ghost.host}/middle/proposal?proid=${proid}&mode=test\n디자이너 : https://${address.backinfo.host}/designer?desid=${desid}`, channel: "#400_customer" });
       await instance.kakao.sendTalk("designerSelect", name, phone, {
         client: name,
         designer: designer,
