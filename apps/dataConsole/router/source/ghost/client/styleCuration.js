c37389d3ec6382966df5f978f6b08a5c87a9d9d7
uragen <uragenbooks@gmail.com>
2021-07-22 15:17:21 +0900

    Robot_210722_v4

--- apps/dataConsole/router/source/ghost/client/styleCuration.js
+++ apps/dataConsole/router/source/ghost/client/styleCuration.js
@@ -3178,6 +3178,12 @@ StyleCurationJs.prototype.insertAdditionBox = function () {
   plusWidth = <%% 24, 24, 24, 21, 4 %%>;
   plusBottom = <%% 39, 48, 47, 41, 33 %%>;
 
+  if (desktop) {
+    if (!isMac()) {
+      plusBottom = plusBottom + 5;
+    }
+  }
+
   titleTopVisualFirst = <%% -2, -3, -4, -4, 0 %%>;
   titleTopVisualSecond = <%% -1, -4, -5, -4, 0 %%>;
 
