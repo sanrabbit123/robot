03d37b31ded291aa39d33c0c3bb60acf0e202390
uragen <uragenbooks@gmail.com>
2021-08-02 18:15:42 +0900

    Robot_210802_v16

--- .office.config.js
+++ .office.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   apps: [
     {
+      name: "robot",
+      script: "./robot.js",
+      instances: 1,
+      exec_mode: "cluster",
+      args: [ "back" ]
+    },
+    {
       name: "ghost",
       script: "./ghost.js",
       instances: 2,
