949fc7c37a2a0ca9c25fb2425736d700725ea66a
uragen <uragenbooks@gmail.com>
2021-08-09 11:58:43 +0900

    Robot_210809_v3

--- apps/billMaker/billMaker.js
+++ apps/billMaker/billMaker.js
@@ -52,6 +52,7 @@ BillMaker.prototype.createBill = async function (collection, updateQueryArr, opt
 
     tong = main(alive, updateQueryArr);
     for (let { fresh, findQuery, insertEvent } of tong) {
+      console.log(fresh, findQuery, insertEvent);
       rows = await MONGOC.db(`miro81`).collection(collection).find(findQuery).toArray();
       if (rows.length === 0) {
         await insertEvent.call(instance, fresh);
