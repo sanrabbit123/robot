47fd3c85d764e60c41adf86afe57aa462b9f93a9
uragen <uragenbooks@gmail.com>
2021-09-16 19:10:27 +0900

    Robot_210916_v15

--- apps/dataConsole/router/source/local/file.js
+++ apps/dataConsole/router/source/local/file.js
@@ -291,7 +291,7 @@ FileJs.prototype.fileLoad = async function (path) {
                 }).catch((err) => {
                   console.log(err);
                 });
-                window.alert("배달 요청이 완료되었습니다! 슬렉의 #file에서 배송이 도착하면 받으시면 됩니다!")
+                window.alert("배달 요청이 완료되었습니다! 슬렉의 #file에서 배송이 도착하면 받으시면 됩니다! 유효 시간은 지금부터 3시간입니다.");
               }
             }
           }
