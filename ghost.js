0ddbdd1db034d1dadd5c98dea9f333b2ba350c44
uragen <uragenbooks@gmail.com>
2021-10-14 17:45:33 +0900

    Robot_211014_v12

--- ghost.js
+++ ghost.js
@@ -1570,7 +1570,9 @@ Ghost.prototype.ghostRouter = function (needs) {
         const pdfName = htmlName.replace(/\.html$/i, ".pdf");
 
         await fileSystem("write", [ `${static}/${htmlName}`, req.body.html.replace(/__equal__/gi, '=').replace(/__ampersand__/gi, '&') ]);
-        shell.exec(`cat ${shellLink(static)}/${htmlName} | wkhtmltopdf - ${shellLink(static)}/${pdfName};rm -rf ${shellLink(static)}/${htmlName}`);
+        // shell.exec(`cat ${shellLink(static)}/${htmlName} | wkhtmltopdf - ${shellLink(static)}/${pdfName};rm -rf ${shellLink(static)}/${htmlName}`);
+
+        shell.exec(`cat ${shellLink(static)}/${htmlName} | wkhtmltopdf - ${shellLink(static)}/${pdfName}`);
         res.send(JSON.stringify({ pdf: `https://${instance.address.officeinfo.ghost.host}/${pdfName}` }));
 
       } catch (e) {
