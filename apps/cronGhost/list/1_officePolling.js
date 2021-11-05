2627e36defbcfc8bf12dc04835e98186e1417857
uragen <uragenbook@gmail.com>
2021-11-05 22:33:51 +0900

    Robot_211105_v8

--- apps/cronGhost/list/1_officePolling.js
+++ apps/cronGhost/list/1_officePolling.js
@@ -1,144 +1,17 @@
 module.exports = [
   {
+    name: "reflect",
+    time: {
+      hour: 4,
+      minute: 50,
+      second: 10
+    }
+  },
+  {
     name: "mysqlReflect",
     time: {
       hour: 5,
       minute: 30,
-      second: 10
-    }
-  },
-  {
-    name: "reflect0",
-    order: "reflect",
-    time: {
-      hour: 8,
-      minute: 30,
-      second: 10
-    }
-  },
-  {
-    name: "clientActionSyncLocal0",
-    order: "clientActionSyncLocal",
-    time: {
-      hour: 8,
-      minute: 50,
-      second: 10
-    }
-  },
-  {
-    name: "calendarSync0",
-    order: "calendarSync",
-    time: {
-      hour: 9,
-      minute: 30,
-      second: 10
-    }
-  },
-  {
-    name: "clientActionSync0",
-    order: "clientActionSync",
-    time: {
-      hour: 10,
-      minute: 40,
-      second: 10
-    }
-  },
-  {
-    name: "reflect1",
-    order: "coreReflect",
-    time: {
-      hour: 12,
-      minute: 50,
-      second: 10
-    }
-  },
-  {
-    name: "clientActionSyncLocal1",
-    order: "clientActionSyncLocal",
-    time: {
-      hour: 13,
-      minute: 10,
-      second: 10
-    }
-  },
-  {
-    name: "calendarSync1",
-    order: "calendarSync",
-    time: {
-      hour: 13,
-      minute: 30,
-      second: 10
-    }
-  },
-  {
-    name: "clientActionSync1",
-    order: "clientActionSync",
-    time: {
-      hour: 14,
-      minute: 30,
-      second: 10
-    }
-  },
-  {
-    name: "reflect2",
-    order: "coreReflect",
-    time: {
-      hour: 15,
-      minute: 50,
-      second: 10
-    }
-  },
-  {
-    name: "clientActionSyncLocal2",
-    order: "clientActionSyncLocal",
-    time: {
-      hour: 16,
-      minute: 10,
-      second: 10
-    }
-  },
-  {
-    name: "calendarSync2",
-    order: "calendarSync",
-    time: {
-      hour: 16,
-      minute: 20,
-      second: 10
-    }
-  },
-  {
-    name: "clientActionSync2",
-    order: "clientActionSync",
-    time: {
-      hour: 18,
-      minute: 20,
-      second: 10
-    }
-  },
-  {
-    name: "reflect3",
-    order: "coreReflect",
-    time: {
-      hour: 21,
-      minute: 10,
-      second: 10
-    }
-  },
-  {
-    name: "clientActionSyncLocal3",
-    order: "clientActionSyncLocal",
-    time: {
-      hour: 21,
-      minute: 30,
-      second: 10
-    }
-  },
-  {
-    name: "calendarSync3",
-    order: "calendarSync",
-    time: {
-      hour: 21,
-      minute: 50,
       second: 10
     }
   }
