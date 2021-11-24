76d8684dc9c0c81c458e89280c1a4b9c7085630c
uragen <uragenbooks@gmail.com>
2021-11-24 14:07:54 +0900

    Robot_211124_v3

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -1551,6 +1551,10 @@ DevContext.prototype.launching = async function () {
       let dateArr;
       let tempClient, tempProject;
       let tempCliidArr;
+      let startDate, endDate;
+      let acc0, acc1, acc2, acc3, acc4, acc5;
+      let curr0, curr1, curr2, curr3, curr4, curr5;
+      let temp;
 
       clients = (await back.getClientsByQuery({}, { selfMongo, withTools: true })).getRequestsTong().map((arr) => { let obj = arr[0].toNormal(); obj.cliid = arr.cliid; return obj; });
       clients.sort((a, b) => { return a.timeline.valueOf() - b.timeline.valueOf(); });
@@ -1582,10 +1586,18 @@ DevContext.prototype.launching = async function () {
       dateArr.reverse();
 
 
-      projectsRaw = await back.getProjectsByQuery({ desid: { $regex: "^d" } }, { selfMongo });
-      projects = projectsRaw.toNormal().filter((obj) => {  return obj.process.contract.first.date.valueOf() >= (new Date(2000, 0, 1)).valueOf() });
+      projectsRaw = (await back.getProjectsByQuery({ desid: { $regex: "^d" } }, { selfMongo })).toNormal();
+      for (let obj of projectsRaw) {
+        for (let history of totalClientHistories) {
+          if (obj.cliid === history.cliid) {
+            obj.manager = history.manager;
+          }
+        }
+      }
+      projects = projectsRaw.filter((obj) => {  return obj.process.contract.first.date.valueOf() >= (new Date(2000, 0, 1)).valueOf() });
       secondCliidArr = projects.map((obj) => { return obj.cliid });
       targetClients = clients.filter((obj) => { return secondCliidArr.includes(obj.cliid); });
+
 
       arr = targetClients.map((obj) => { return { cliid: obj.cliid, manager: obj.manager } });
       result = {};
@@ -1611,49 +1623,103 @@ DevContext.prototype.launching = async function () {
       totalLength = Object.values(total).map((arr) => { return arr.length; }).reduce((accumulate, current) => { return accumulate + current });
       resultLength = Object.values(result).map((arr) => { return arr.length; }).reduce((accumulate, current) => { return accumulate + current });
       keys.sort();
+      acc0 = 0;
+      acc1 = 0;
+      acc2 = 0;
+      acc3 = 0;
+      acc4 = 0;
+      acc5 = 0;
       for (let name of keys) {
+        curr0 = total[name].length;
+        curr1 = result[name].length;
+        curr2 = result[name].length;
+        curr3 = total[name].length === 0 ? 0 : (Math.floor((result[name].length / total[name].length) * 100000) / 1000);
+        curr4 = totalLength === 0 ? 0 : (Math.floor((total[name].length / totalLength) * 100000) / 1000);
+        curr5 = resultLength === 0 ? 0 : (Math.floor((result[name].length / resultLength) * 100000) / 1000);
+
+        acc0 += curr0;
+        acc1 += curr1;
+        acc2 += curr2;
+        acc3 += curr3;
+        acc4 += curr4;
+        acc5 += curr5;
+
         arr = [];
         arr.push(name);
-        arr.push(total[name].length);
-        arr.push(result[name].length);
-        arr.push(result[name].length);
-        arr.push(total[name].length === 0 ? 0 : (Math.round((result[name].length / total[name].length) * 10000) / 100));
-        arr.push(totalLength === 0 ? 0 : (Math.round((total[name].length / totalLength) * 10000) / 100));
-        arr.push(resultLength === 0 ? 0 : (Math.round((result[name].length / resultLength) * 10000) / 100));
+        arr.push(curr0);
+        arr.push(curr1);
+        arr.push(curr2);
+        arr.push(curr3);
+        arr.push(curr4);
+        arr.push(curr5);
         matrix.push(arr);
       }
 
-      console.log(dateArr);
+      matrix.push([ "total", acc0, acc1, acc2, (Math.floor((acc1 / acc0) * 100000) / 1000), Math.round(acc4), Math.round(acc5) ]);
+
+      matrix.push([ "", "", "", "", "", "", "" ]);
+
       for (let [ year, month ] of dateArr) {
+
+        startDate = new Date(year, month - 1, 1);
+        if (month < 12) {
+          endDate = new Date(year, month, 1);
+        } else {
+          endDate = new Date(year + 1, 0, 1);
+        }
+
         matrix.push([ "담당자명", String(year).slice(2) + '.' + String(month) + " 응대수", String(year).slice(2) + '.' + String(month) + " 문의자 계약수", String(year).slice(2) + '.' + String(month) + " 계약자 수", "개인계약 성사율(%)", "팀내 응대 비율(%)", "팀내 계약 비율(%)" ]);
 
-        tempClient = equalJson(JSON.stringify(clients));
-        tempProject = projectsRaw.toNormal();
-
-        tempClient = tempClient.filter((obj) => { return obj.timeline.getFullYear() === year && obj.timeline.getMonth() + 1 === month });
+        tempClient = equalJson(JSON.stringify(clients)).filter((obj) => { return obj.timeline.valueOf() >= startDate.valueOf() && obj.timeline.valueOf() < endDate.valueOf() });
+        tempProject = equalJson(JSON.stringify(projectsRaw));
         tempCliidArr = tempClient.map((obj) => { return obj.cliid });
+        acc0 = tempClient.length;
+        acc1 = tempProject.filter((obj) => { return tempCliidArr.includes(obj.cliid); }).length;
 
+        for (let name of keys) {
+          tempClient = equalJson(JSON.stringify(clients)).filter((obj) => { return obj.timeline.valueOf() >= startDate.valueOf() && obj.timeline.valueOf() < endDate.valueOf() }).filter((obj) => { return obj.manager === name });
+          tempProject = equalJson(JSON.stringify(projectsRaw));
+          tempCliidArr = tempClient.map((obj) => { return obj.cliid });
+          curr0 = tempClient.length;
+          curr1 = tempProject.filter((obj) => { return tempCliidArr.includes(obj.cliid); }).length;
+          curr2 = tempProject.filter((obj) => { return obj.process.contract.first.date.valueOf() >= startDate.valueOf() && obj.process.contract.first.date.valueOf() < endDate.valueOf() }).filter((obj) => { return obj.manager === name }).length;
+          curr3 = curr0 === 0 ? 0 : Math.floor((curr1 / curr0) * 100000) / 1000;
+          curr4 = acc0 === 0 ? 0 : Math.floor((curr0 / acc0) * 100000) / 1000;
+          curr5 = acc1 === 0 ? 0 : Math.floor((curr1 / acc1) * 100000) / 1000;
+          matrix.push([
+            name,
+            curr0,
+            curr1,
+            curr2,
+            curr3,
+            curr4,
+            curr5,
+          ]);
+        }
+
+        tempClient = equalJson(JSON.stringify(clients)).filter((obj) => { return obj.timeline.valueOf() >= startDate.valueOf() && obj.timeline.valueOf() < endDate.valueOf() });
+        tempProject = equalJson(JSON.stringify(projectsRaw));
+        tempCliidArr = tempClient.map((obj) => { return obj.cliid });
+        curr0 = tempClient.length;
+        curr1 = tempProject.filter((obj) => { return tempCliidArr.includes(obj.cliid); }).length;
+        curr2 = tempProject.filter((obj) => { return obj.process.contract.first.date.valueOf() >= startDate.valueOf() && obj.process.contract.first.date.valueOf() < endDate.valueOf() }).length;
+        curr3 = curr0 === 0 ? 0 : Math.floor((curr1 / curr0) * 100000) / 1000;
+        curr4 = 100;
+        curr5 = 100;
         matrix.push([
-          "전체",
-          tempClient.length,
-          tempProject.filter((obj) => { return tempCliidArr.includes(obj.cliid); }).length,
-          tempProject.filter((obj) => { return obj.process.contract.first.date.getFullYear() === year && obj.process.contract.first.date.getMonth() + 1 === month }).length
+          "total",
+          curr0,
+          curr1,
+          curr2,
+          curr3,
+          curr4,
+          curr5,
         ]);
+        matrix.push([ "", "", "", "", "", "", "" ]);
 
       }
 
-
-
-
-
-
-
-
-
-      // await sheets.update_value_inPython(sheetsId, "", matrix);
-
-      console.log(matrix);
-
+      console.log(await sheets.update_value_inPython(sheetsId, "", matrix));
 
     }
 
