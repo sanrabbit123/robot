04dc7bab7ff2f2862aea4127990edf685584f7be
uragen <uragenbooks@gmail.com>
2021-12-04 00:27:18 +0900

    Robot_211203_v8

--- apps/publicSector/sector/router/router.py
+++ apps/publicSector/sector/router/router.py
@@ -2,19 +2,12 @@ from router.mother import *
 from aiohttp import web
 from pymongo import MongoClient
 
-# from router.get.status import getStatus
-# from router.get.base import getBase
+=> module
 
 routes = web.RouteTableDef()
 mongoConnection = MongoClient("mongodb://localhost:27017/")
 
-# @routes.get('/info/status')
-# async def get_status(request):
-#     return web.json_response(await getStatus(request, mongoConnection))
-#
-# @routes.get('/{name}')
-# async def get_base(request):
-#     return web.Response(content_type="text/html", text=getBase(request, mongoConnection))
+=> function
 
 def routerReturn():
     return routes
