dbd7a9d27e86c1c6292c1fda5a6f4e391ad8922d
uragen <uragenbooks@gmail.com>
2021-12-08 18:49:11 +0900

    Robot_211208_v7

--- apps/publicSector/sector/router/post/excel.py
+++ apps/publicSector/sector/router/post/excel.py
@@ -8,20 +8,19 @@ STATIC_PATH = "/home/homeliaison/samba"
 async def postExcel(request, mongoConnection):
     data = await request.post()
 
-    # dic = pandas.read_excel(STATIC_PATH + data["file"], sheet_name=None)
-    # arr = dic[data["sheetsName"]].values.tolist()
-    #
-    # columns = dic[data["sheetsName"]].columns.tolist()
-    # firstArr = []
-    # for i in columns:
-    #     if re.search("Unnamed: ", i) is None:
-    #         firstArr.append(i)
-    #     else:
-    #         firstArr.append(None)
-    # arr.insert(0, firstArr)
-    # j = json.dumps(arr, ensure_ascii=False).replace("NaN", "null")
-    # return json.loads(j)
-    return { "message": "ok" }
+    dic = pandas.read_excel(STATIC_PATH + data["file"], sheet_name=None)
+    arr = dic[data["sheetsName"]].values.tolist()
+
+    columns = dic[data["sheetsName"]].columns.tolist()
+    firstArr = []
+    for i in columns:
+        if re.search("Unnamed: ", i) is None:
+            firstArr.append(i)
+        else:
+            firstArr.append(None)
+    arr.insert(0, firstArr)
+    j = json.dumps(arr, ensure_ascii=False).replace("NaN", "null")
+    return json.loads(j)
 
 
 => @routes.post("/excel")
