89a123a536f61e5edfa46fb1e88a8e15f70d1e21
uragen <uragenbooks@gmail.com>
2024-06-11 09:41:41 +0900

    Rohan_240611_v1

--- apps/secondGhost/router/secondRouter.js
+++ apps/secondGhost/router/secondRouter.js
@@ -301,14 +301,15 @@ SecondRouter.prototype.rou_post_messageLog = function () {
 
       }
 
-      setQueue(() => {
-        if (!fairyMode) {
-          instance.slack_bot.chat.postMessage({ text: slackText, channel: (channel === "silent" ? "#error_log" : channel) }).catch((err) => { console.log(err); });
-        } else {
-          instance.slack_fairy.chat.postMessage({ text: slackText, channel: (channel === "silent" ? "#error_log" : channel) }).catch((err) => { console.log(err); });
-        }
-      }, 0)
-
+      if (!/alive_log/gi.test(channel) && !/cron_log/gi.test(channel) && !/error_log/gi.test(channel)) {
+        setQueue(() => {
+          if (!fairyMode) {
+            instance.slack_bot.chat.postMessage({ text: slackText, channel: (channel === "silent" ? "#error_log" : channel) }).catch((err) => { console.log(err); });
+          } else {
+            instance.slack_fairy.chat.postMessage({ text: slackText, channel: (channel === "silent" ? "#error_log" : channel) }).catch((err) => { console.log(err); });
+          }
+        }, 0)
+      }
 
       res.send(JSON.stringify({ message: "will do" }));
     } catch (e) {
