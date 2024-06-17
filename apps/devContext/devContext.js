42dee6a9cbc323bab2047b41ecfda019f90f2784
uragen <uragenbooks@gmail.com>
2024-06-17 17:54:57 +0900

    Rohan_240617_v1

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -7177,8 +7177,8 @@ DevContext.prototype.launching = async function () {
 
     
     // send sms
-    // const name = "이은지";
-    // const amount = 1_474_000;
+    // const name = "이국화";
+    // const amount = 3_009_000;
     // await human.sendSms({
     //   to: "01055432039",
     //   body: dateToString(new Date(), true).replace(/\-/gi, '/').slice(0, -3) + `\n입금 ${autoComma(amount)}원\n잔액 0원\n${name}\n049***56704022\n기업`,
