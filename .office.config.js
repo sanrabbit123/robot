5965643af7cabda7a2f50fb224fb68c324fe53f6
uragen <uragenbooks@gmail.com>
2024-06-20 18:11:10 +0900

    Rohan_240620_v2

--- .office.config.js
+++ .office.config.js
@@ -1,16 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "alien",
-      script: "./alien.js",
-      instances: 1,
-      exec_mode: "cluster",
-      args: [ "receiveSms" ]
-    },
-    {
       name: "robot",
       script: "./robot.js",
-      instances: 6,
+      instances: 2,
       exec_mode: "cluster",
       args: [ "static" ]
     },
