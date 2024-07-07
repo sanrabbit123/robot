f4a8b3484bbd3ab582d951a51fd51783cccd1a9b
uragen <uragenbooks@gmail.com>
2024-07-07 15:48:24 +0900

    Rohan_240707_v4

--- apps/devContext/devContext.js
+++ apps/devContext/devContext.js
@@ -7378,15 +7378,15 @@ DevContext.prototype.launching = async function () {
     // });
 
     // raw photo to raw portfolio
-    // const filter = new PortfolioFilter();
-    // await filter.rawToRaw([
-    //   {
-    //     client: "한수미",
-    //     designer: "김소영",
-    //     link: "https://drive.google.com/drive/folders/1U7HDUv2wgyanB6KiVFqtBNp8t2GoZrkG",
-    //     pay: true
-    //   },
-    // ]);
+    const filter = new PortfolioFilter();
+    await filter.rawToRaw([
+      {
+        client: "한수미",
+        designer: "김소영",
+        link: "https://drive.google.com/drive/folders/1U7HDUv2wgyanB6KiVFqtBNp8t2GoZrkG",
+        pay: true
+      },
+    ]);
 
     // raw video
     // const filter = new PortfolioFilter();
