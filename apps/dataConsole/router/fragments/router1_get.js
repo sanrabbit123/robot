0dd38360af11a193f77c50c7d38b7bb7b39dd038
uragen <uragenbooks@gmail.com>
2024-07-23 10:49:23 +0900

    Rohan_240723_v2

--- apps/dataConsole/router/fragments/router1_get.js
+++ apps/dataConsole/router/fragments/router1_get.js
@@ -224,7 +224,7 @@ DataRouter.prototype.rou_get_First = function () {
 
       } else {
 
-        if (!pass) {
+        if (false) {
 
           res.set("Content-Type", "text/html");
           res.send(`<html><head><title>알 수 없는 ip</title></head><body><script>
