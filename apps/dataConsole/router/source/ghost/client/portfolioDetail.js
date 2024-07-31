7f85435018838d6b5efcf1bbb5633cd8da74799f
uragen <uragenbooks@gmail.com>
2024-07-31 13:04:24 +0900

    Rohan_240731_v8

--- apps/dataConsole/router/source/ghost/client/portfolioDetail.js
+++ apps/dataConsole/router/source/ghost/client/portfolioDetail.js
@@ -1255,7 +1255,7 @@ PortfolioDetailJs.prototype.portfolioContentsBox = async function (updatedConten
                     await ajaxForm(formData, "https://" + FILEHOST + ":3001/replaceContentsPhoto", loading.progress);
                     GeneralJs.sleep(2000).then(() => {
                       loading.remove();
-                      window.location.href = window.location.href + "&eraseCache=true";
+                      window.location.reload(true);
                     }).catch((err) => { console.log(err); })
                   }
                 }
