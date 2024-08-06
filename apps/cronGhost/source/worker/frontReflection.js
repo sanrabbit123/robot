cbe17b65943543a735165bdb4a0e142de6b9c249
uragen <uragenbooks@gmail.com>
2024-08-06 09:56:54 +0900

    Rohan_240806_v6

--- apps/cronGhost/source/worker/frontReflection.js
+++ apps/cronGhost/source/worker/frontReflection.js
@@ -8,7 +8,8 @@ const worker = async function (package) {
   const { mother, address, back, mongo, mongolocal } = package;
   const { requestSystem, messageLog, errorLog, emergencyAlarm } = mother;
   try {
-    await requestSystem("https://" + address.testinfo.host + ":" + String(3000) + "/frontReflection", { data: null }, { headers: { "Content-Type": "application/json" } });
+    await requestSystem("https://" + address.officeinfo.ghost.host + ":" + String(3000) + "/frontReflection", { data: null }, { headers: { "Content-Type": "application/json" } });
+    await requestSystem("https://" + address.officeinfo.ghost.host + ":3000/syncContentsTag", { data: null }, { headers: { "Content-Type": "application/json" } });
     return true;
   } catch (e) {
     await emergencyAlarm("front reflection error : " + e.message);
