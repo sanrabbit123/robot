d364a69de6d595255b4f2ac481936056cbb3b394
uragen <uragenbooks@gmail.com>
2024-08-18 10:44:34 +0900

    Rohan_240818_v11

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/robot;git reset --hard 904be2f938fdf02ffed3820a192161993d59cc52;git pull;node /home/ubuntu/robot/robot.js infoObj;cd /home/ubuntu/robot;git pull;pm2 kill;pm2 start /home/ubuntu/robot/.office.config.js;systemctl restart robot-cron;systemctl restart robot-trans;
+cd /home/ubuntu/robot;git reset --hard 904be2f938fdf02ffed3820a192161993d59cc52;git pull;node /home/ubuntu/robot/robot.js infoObj;cd /home/ubuntu/robot;git pull;pm2 kill;pm2 start /home/ubuntu/robot/.office.config.js;systemctl restart robot-cron;systemctl restart robot-trans;systemctl restart nginx;
